b13005e3fd6ffadcd4fdfc44f70a7e74769da609
Rico Pahlisch <rico.pahlisch@kiwigrid.com>
2021-06-01 17:51:01 +0200

    fix

--- images/centos/scripts/configure_network.sh
+++ images/centos/scripts/configure_network.sh
@@ -9,7 +9,7 @@ localectl set-locale LANG=en_US.UTF-8
 localectl set-locale LANGUAGE=en_US.UTF-9
 
 # install hetzner cloud networks configuration package
-curl https://packages.hetzner.com/hcloud/rpm/hc-utils-0.0.3-1.el8.noarch.rpm -o /tmp/hc-utils-0.0.3-1.el7.noarch.rpm -s
+curl https://packages.hetzner.com/hcloud/rpm/hc-utils-0.0.3-1.el8.noarch.rpm -o /tmp/hc-utils-0.0.3-1.el8.noarch.rpm -s
 dnf -y install /tmp/hc-utils-0.0.3-1.el8.noarch.rpm
 
 # disable public interface
