a27923e1912afa19a0b6a24dcc417027e1721969
Rico Pahlisch <rico.pahlisch@kiwigrid.com>
2021-08-06 15:23:59 +0200

    add priv net test

--- images/centos/scripts/configure_network.sh
+++ images/centos/scripts/configure_network.sh
@@ -20,10 +20,36 @@ Wants=network-online.target
 After=network-online.target
 
 [Service]
-ExecStart=/bin/sh -c 'nmcli connection down "System eth0" && sed -i -e '/^ONBOOT/s/^.*$/ONBOOT=false/' /etc/sysconfig/network-scripts/ifcfg-eth0 && ip route add default via 10.0.0.1 dev ens10'
+ExecStart=/bin/sh -c 'nmcli connection down "System eth0" && sed -i -e '/^ONBOOT/s/^.*$/ONBOOT=false/' /etc/sysconfig/network-scripts/ifcfg-eth0'
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+cat > /usr/bin/setup_gateway.sh <<EOF
+#!/bin/sh
+
+ip route add default via 10.0.0.1 dev ens10
+
+while [ $? -ne 0 ]; do
+    ip route add default via 10.0.0.1 dev ens10
+done
+EOF
+
+chmod +x /usr/bin/setup_gateway.sh
+
+cat > /etc/systemd/system/setup-default-gateway.service <<EOF
+[Unit]
+Description=Disable Public Interface
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+ExecStart=/bin/sh -c '/usr/bin/setup_gateway.sh'
 
 [Install]
 WantedBy=multi-user.target
 EOF
 
 systemctl enable disable-public-interface.service
+systemctl enable setup-default-gateway.service
